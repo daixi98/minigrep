993a9ba3f0c2c1777fd704dda1f9b7bb7d6f9861
daixi98 <x3dai@ucsd.edu>
2022-04-03 17:47:05 -0700

    done

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,16 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-
-    println!("{}, {}", config.query, config.filename);
 
     //let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
     //println!("With text:\n{}", contents);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     };
